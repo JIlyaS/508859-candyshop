16a440d20aa524b3ddd4ad62130226277285040e
Ilya Kolmakov <ilya.kolmakov.i@mail.ru>
2018-10-15 23:11:28 +0500

    Final check v1.4

--- js/delivery.js
+++ js/delivery.js
@@ -2,9 +2,9 @@
 
 (function () {
   var storeMapImg = document.querySelector('.deliver__store-map-img');
-  window.order.deliverStore.addEventListener('click', deliverChangeHandler);
+  window.order.deliverStore.addEventListener('click', deliverClickHandler);
 
-  function deliverChangeHandler(evt) {
+  function deliverClickHandler(evt) {
     var mapName = window.data.DELIVERY_MAP_NAMES[evt.target.innerText];
     if (mapName) {
       storeMapImg.src = window.data.PATH_PICTURES + mapName + window.data.EXTENDS;
