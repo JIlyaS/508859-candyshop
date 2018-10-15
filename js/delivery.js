5a136ba1201ecdc0d4f9b88c9af7bba80a91d9e0
Ilya Kolmakov <ilya.kolmakov.i@mail.ru>
2018-10-15 23:48:23 +0500

    delivery

---
+++ js/delivery.js
@@ -0,0 +1,15 @@
+'use strict';
+
+(function () {
+  var storeMapImg = document.querySelector('.deliver__store-map-img');
+  window.order.deliverStore.addEventListener('click', deliverChangeHandler);
+
+  function deliverChangeHandler(evt) {
+    var mapName = window.data.DELIVERY_MAP_NAMES[evt.target.innerText];
+    if (mapName) {
+      storeMapImg.src = window.data.PATH_PICTURES + mapName + window.data.EXTENDS;
+      storeMapImg.alt = window.data.DELIVERY_MAP_NAMES[evt.target.innerText];
+    }
+  }
+
+})();
